4df38404fdc6b19f98a82fb5c7139eda7fab9cb0
Sourabh Sriom <sourabh.iitg@gmail.com>
2017-11-06 19:14:17 -0800

    pep-8 fixes

--- newsdb.py
+++ newsdb.py
@@ -4,37 +4,37 @@ import datetime
 import psycopg2
 import bleach
 
-#setting the database name 
+# setting the database name
 DBNAME = "news"
 
+
 def get_top_authors():
-
-  db = psycopg2.connect(database=DBNAME)
-  cursor = db.cursor()
-
-  query = '''select * from best_authors;'''
-  cursor.execute(query)
-
-  return cursor.fetchall()
-  db.close()
-
-def get_top_articles():
-
-  db = psycopg2.connect(database=DBNAME)
-  cursor = db.cursor()
-
-  query = '''select * from best_articles;'''
-  cursor.execute(query)
-  return cursor.fetchall()
-  db.close()
-
-def get_fails():
-
 
     db = psycopg2.connect(database=DBNAME)
     cursor = db.cursor()
 
+    query = '''select * from best_authors;'''
+    cursor.execute(query)
 
+    return cursor.fetchall()
+    db.close()
+
+
+def get_top_articles():
+
+    db = psycopg2.connect(database=DBNAME)
+    cursor = db.cursor()
+
+    query = '''select * from best_articles;'''
+    cursor.execute(query)
+    return cursor.fetchall()
+    db.close()
+
+
+def get_fails():
+
+    db = psycopg2.connect(database=DBNAME)
+    cursor = db.cursor()
     query = '''select visits.date as date,
         round(cast(cast(fails.count as float)
         /cast(visits.count as float) * 100 as numeric), 2)
