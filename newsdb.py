0e5111eb1b380bc12dfc777c312f3b65f1f8bbff
Sourabh Sriom <sourabh.iitg@gmail.com>
2017-11-10 22:36:24 -0800

    reviewed changed

---
+++ newsdb.py
@@ -0,0 +1,48 @@
+# "Database code" for the reporting tool
+
+import datetime
+import psycopg2
+import bleach
+
+# setting the database name
+DBNAME = "news"
+
+
+def get_top_authors():
+
+    db = psycopg2.connect(database=DBNAME)
+    cursor = db.cursor()
+
+    query = '''select * from best_authors limit 3;'''
+    cursor.execute(query)
+    db.close()
+    return cursor.fetchall()
+
+
+def get_top_articles():
+
+    db = psycopg2.connect(database=DBNAME)
+    cursor = db.cursor()
+
+    query = '''select * from best_articles limit 3;'''
+    cursor.execute(query)
+    db.close()
+    return cursor.fetchall()
+
+
+def get_fails():
+
+    db = psycopg2.connect(database=DBNAME)
+    cursor = db.cursor()
+    query = '''select visits.date as date,
+        round(cast(cast(fails.count as float)
+        /cast(visits.count as float) * 100 as numeric), 2)
+        as errors
+        from visits
+        inner join fails
+        on visits.date = fails.date
+        where cast(fails.count as float)/cast(visits.count as float) > 0.01;'''
+
+    cursor.execute(query)
+    db.close()
+    return cursor.fetchall()
---
+++ report.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+
+from newsdb import get_top_authors, get_top_articles, get_fails
+
+
+def main():
+
+    print("\n \t Here are the best authors in our library : \n")
+    for author in get_top_authors():
+        print (" \t  * \t" + str(author[0]) + " - "
+               + str(author[1]) + " views")
+
+    print("\n \t Here are the top articles of our library : \n")
+    for article in get_top_articles():
+        print (''' \t  * \t \"''' + str(article[0]) + '''\" - ''' +
+               str(article[1]) + ''' views''')
+
+    print("\n \t Here are the days of excess access failures! : \n")
+    for fails in get_fails():
+        print (" \t  * \t" + str(fails[0]) + "--" + str(fails[1])
+               + "% failures \n")
+
+if __name__ == "__main__":
+
+    main()
